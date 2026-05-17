223c89d7912595435b2011bd9a47002fd7716b05
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-05-18 01:08:04 +0530

    feat: add yolo training script

---
+++ scripts/yolov8_training.py
@@ -0,0 +1,87 @@
+from ultralytics import YOLO
+import torch
+import os
+from config import YOLO_MODEL_NAME
+
+DATA_PATH = "./dataset/VehicleCount/data.yaml"
+
+def train_yolov8():
+    # Load the model
+    model = YOLO(YOLO_MODEL_NAME)
+    print(f"Model loaded: {YOLO_MODEL_NAME}")
+    # Train
+    results = model.train(
+        data=DATA_PATH,
+        epochs=50,
+        imgsz=640,
+        device=0,
+        batch=16,
+        workers=4,
+        single_cls=True  # Optional: Forces the model to treat data as single-class
+    )
+    print("Training complete.")
+
+def evaluate_yolov8(model_path):
+    # model = YOLO(YOLO_MODEL_NAME)
+    model = YOLO(model_path)
+
+    # Run validation on the 'test' split
+    metrics = model.val(
+        data=DATA_PATH,
+        split='test'  # <--- Critical: tells it to use the test set
+    )
+
+    print(f"Test mAP50: {metrics.box.map50}")
+    print(f"Test mAP50-95: {metrics.box.map}")
+
+def predict(model_path, test_images_dir):
+    # 1. Load the model
+    # model = YOLO(YOLO_MODEL_NAME)
+    model = YOLO(model_path)
+
+    # 2. Run prediction on the whole folder
+    SAVE_DIR = f"./results/test"
+    os.makedirs(SAVE_DIR, exist_ok=True)
+    results = model.predict(source=test_images_dir, save=True, conf=0.5, iou=0.65, project=os.path.dirname(SAVE_DIR), name=os.path.basename(SAVE_DIR))
+
+    print(f"Check the folder: {results[0].save_dir}")
+
+def count_objects(model_path, image_path):
+    # Load the model
+    # model = YOLO(YOLO_MODEL_NAME)
+    model = YOLO(model_path)
+
+    # Run inference
+    results = model(image_path, conf=0.3, iou=0.65, verbose=False)
+    
+    # Get the first result (since we only passed one image)
+    detections = results[0]
+
+    # CHECK: Is this an OBB model or a Normal Box model?
+    if detections.obb is not None:
+        # It's an OBB model (Rotated Boxes)
+        object_count = len(detections.obb)
+    else:
+        # It's a Standard model (Rectangles)
+        object_count = len(detections.boxes)
+    
+    return object_count
+
+if __name__ == "__main__":
+    print(f'GPU Available: {torch.cuda.is_available()}')
+    print(torch.__version__)
+    # train_yolov8()
+    # evaluate_yolov8("runs/detect/train-2/weights/best.pt")
+    # predict("runs/detect/train-2/weights/best.pt", "test_img.jpg")
+    count = count_objects("runs/detect/train-2/weights/best.pt", "test_img.jpg")
+    print(count)
+    # img_num = 24
+    # for i in os.listdir("./frames"):
+    #     img_num = i[-2:]
+    #     predict(f"data/models/IMG_00{img_num}/img_00{img_num}.pt", f"data/models/IMG_00{img_num}/frames", img_num)
+    # image_dir = "./frames/IMG_0028"
+    # for img_file in os.listdir(image_dir):
+    #     if img_file.endswith(('.png', '.jpg', '.jpeg')):
+    #         image_path = os.path.join(image_dir, img_file)
+    #         count = count_objects(image_path, "runs/obb/train10/weights/best.pt")
+    #         print(f"Image: {img_file}, Object Count: {count}")
